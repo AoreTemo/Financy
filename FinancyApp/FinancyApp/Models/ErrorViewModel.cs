8a098062ed586a805d5fe0a48b83d14457c8e937
Artem <temaprgod8@gmail.com>
2023-12-09 22:56:05 +0200

    asp project init

---
+++ FinancyApp/FinancyApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FinancyApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
